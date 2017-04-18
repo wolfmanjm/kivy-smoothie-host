12f274285e31ec2eaac4c75f64b80854ac345c05
Jim Morris <morris@wolfman.com>
2017-04-18 00:19:09 -0700

    cleaner layout nicer keyboard

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from kivy.app import App
 
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from kivy.app import App
@@ -16,27 +16,24 @@ from kivy.lang import Builder
 
 Builder.load_string('''
 # a template Butt of type Button
-[Butt@Button]
-    # ctx.'attribute_name' is used to access the
-    # attributes defined in the instance of Butt.
-    text: ctx.text
-    # below vars are constant for every instance of Butt
-    size_hint_x: None
-    width: 57.143
-    on_press: self.parent.parent.do_action(ctx.text)
+<Butt@Button>
+    font_size: 16
+    on_press: self.parent.parent.do_action(self.text)
 
 <KbdWidget>:
-    GridLayout:
-        cols: 7
-        row_force_default: True
-        row_default_height: 30
-        #pos_hint: {'center_x':.5}
-        size_hint: (None, None)
-        # size is updated whenever minimum_size is.
-        size: self.minimum_size
-        # top is updated whenever height is.
-        top: self.height
+	display: entry
+    rows: 9
+    padding: 10
+    spacing: 10
 
+    BoxLayout:
+	    TextInput:
+	    	id: entry
+	    	multiline: False
+	    	font_size: 16
+	    	on_text_validate: self.parent.parent.handle_input(self.text)
+
+	BoxLayout:
         Butt:
         	text: 'G'
         Butt:
@@ -45,6 +42,8 @@ Builder.load_string('''
         	text: 'T'
         Butt:
         	text: 'F'
+
+    BoxLayout:
         Butt:
         	text: 'X'
         Butt:
@@ -52,15 +51,17 @@ Builder.load_string('''
         Butt:
         	text: 'Z'
         Butt:
+        	text: 'E'
+
+	BoxLayout:
+        Butt:
         	text: 'A'
         Butt:
         	text: 'B'
         Butt:
         	text: 'C'
         Butt:
-        	text: 'D'
-        Butt:
-        	text: 'E'
+    		text: 'D'
         Butt:
         	text: 'H'
         Butt:
@@ -69,6 +70,8 @@ Builder.load_string('''
         	text: 'J'
         Butt:
         	text: 'K'
+
+    BoxLayout:
         Butt:
         	text: 'L'
         Butt:
@@ -85,14 +88,8 @@ Builder.load_string('''
         	text: 'V'
         Butt:
         	text: 'W'
-        Butt:
-        	text: ' '
-	    Butt:
-        	text: ' '
-    	Butt:
-        	text: ' '
-    	Butt:
-        	text: ' '
+
+    BoxLayout:
         Butt:
             text: '1'
         Butt:
@@ -101,6 +98,8 @@ Builder.load_string('''
             text: '3'
         Butt:
             text: '4'
+
+    BoxLayout:
         Butt:
             text: '5'
         Butt:
@@ -109,6 +108,8 @@ Builder.load_string('''
             text: '7'
         Butt:
             text: '8'
+
+    BoxLayout:
         Butt:
             text: '9'
         Butt:
@@ -117,6 +118,8 @@ Builder.load_string('''
             text: '.'
         Butt:
             text: '-'
+
+    BoxLayout:
         Butt:
         	text: 'BS'
         Butt:
@@ -132,49 +135,34 @@ Builder.load_string('''
 <MainWindow>:
 	orientation: 'horizontal'
 
-	BoxLayout:
-		orientation: 'vertical'
-		padding: 1,1,0,0
-		#spacing: (5, 5)
+	ScrollableLabel:
+		text: kbd_widget.log
 
-		ScrollableLabel:
-			text: kbd_widget.log
+	KbdWidget:
+		id: kbd_widget
 
-		BoxLayout:
-			orientation: 'vertical'
-		    TextInput:
-		    	id: command_input
-		    	multiline: False
-		    	size_hint_y: None
-		    	height: self.minimum_height
-		    	#pos_hint: {'x': 0, 'y': 0}
-		    	text: kbd_widget.inp
-		    	on_text_validate: kbd_widget.handle_input(self.text)
-
-			KbdWidget:
-				id: kbd_widget
-	Widget:
 ''')
 
 class ScrollableLabel(ScrollView):
-    text = StringProperty('')
+	text= StringProperty()
 
-class KbdWidget(Widget):
-	log = StringProperty()
-	inp = StringProperty()
+class KbdWidget(GridLayout):
+	log= StringProperty()
+
 	def do_action(self, key):
 		print("Key " + key)
 		if key == 'Send':
-			self.log += self.inp + '\n'
-			self.inp= ''
+			print("Sending " + self.display.text)
+			self.log += self.display.text + '\n'
+			self.display.text = ''
 		elif key == 'BS':
-			self.inp= self.inp[:-1]
+			self.display.text = self.display.text[:-1]
 		else:
-			self.inp += key
+			self.display.text += key
 
 	def handle_input(self, s):
-		self.log += s + '\n'
-		self.inp= ''
+		self.log += self.display.text + '\n'
+		self.display.text = ''
 
 class MainWindow(BoxLayout):
 	pass
