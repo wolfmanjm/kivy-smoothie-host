e31f5b482802115343f89063bb0ca343af101ada
Jim Morris <morris@wolfman.com>
2018-08-07 12:43:28 +0100

    better version that handles video from mjpeg streamer correctly

--- camera_screen.py
+++ camera_screen.py
@@ -4,22 +4,26 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
 from kivy.properties import StringProperty
+from kivy.uix.image import Image
+from kivy.core.image import Image as CoreImage
+from collections import deque
+from kivy.logger import Logger, LOG_LEVELS
+import io
+import urllib.request
+import threading
 
 Builder.load_string('''
+<MjpegViewer>:
+    #url: 'http://192.168.1.11:8000/?action=stream'
+    url: 'http://localhost:8080/?action=stream'
+
 <CameraScreen>:
-    on_enter: self.start_timer()
-    on_leave: self.stop_timer()
+    on_enter: self.start()
+    on_leave: self.stop()
     BoxLayout:
         orientation: 'vertical'
-
-        AsyncImage:
-            id: ai
-            size_hint: 0.8, 0.8
-            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-            mipmap: True
-            source: root.url
-            nocache: True
-
+        MjpegViewer:
+            id: viewer
         Button:
             size_hint_y: None
             height: 40
@@ -27,19 +31,65 @@ Builder.load_string('''
             on_press: root.manager.current= 'main'
 ''')
 
+class MjpegViewer(Image):
+
+    url = StringProperty()
+
+    def start(self):
+        self.quit = False
+        self._queue = deque()
+        self._thread = threading.Thread(target=self.read_stream)
+        self._thread.daemon = True
+        self._thread.start()
+        self._image_lock = threading.Lock()
+        self._image_buffer = None
+        self.read_queue= Clock.schedule_interval(self.update_image, 1 / 10.)
+
+    def stop(self):
+        self.quit = True
+        self._thread.join()
+        Clock.unschedule(self.read_queue)
+
+    def read_stream(self):
+        try:
+            stream = urllib.request.urlopen(self.url)
+        except Exception as err:
+            self.quit= True
+            Logger.error("MjpegViewer: Failed to open url: {} - error: {}".format(self.url, err))
+
+        bytes = b''
+        while not self.quit:
+            bytes += stream.read(1024)
+            a = bytes.find(b'\xff\xd8')
+            b = bytes.find(b'\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes[a:b + 2]
+                bytes = bytes[b + 2:]
+
+                data = io.BytesIO(jpg)
+                im = CoreImage(data,
+                               ext="jpeg",
+                               nocache=True)
+                with self._image_lock:
+                    self._image_buffer = im
+
+    def update_image(self, *args):
+        if self.quit:
+            return
+
+        im = None
+        with self._image_lock:
+            im = self._image_buffer
+            self._image_buffer = None
+        if im is not None:
+            self.texture = im.texture
+            self.texture_size = im.texture.size
+
 
 class CameraScreen(Screen):
-    url= StringProperty()
-    def _do_refresh(self, d):
-        try:
-            self.ids.ai.reload()
-        except:
-            self.clk.cancel()
 
-    def start_timer(self):
-        self.url= 'http://192.168.1.11:8000/?action=snapshot' #'http://localhost:8080/?action=snapshot'
-        self.clk= Clock.schedule_interval(self._do_refresh, 1)
+    def start(self):
+        self.ids.viewer.start()
 
-    def stop_timer(self):
-        self.clk.cancel()
-        self.clk= None
+    def stop(self):
+        self.ids.viewer.stop()
