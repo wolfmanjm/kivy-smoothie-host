bc1a962242a131cf988ec848fb0371aeb4d2c1e4
Jim Morris <morris@wolfman.com>
2018-09-11 21:45:19 +0100

    clean up tool change message

--- main.py
+++ main.py
@@ -784,7 +784,7 @@ class MainWindow(BoxLayout):
     @mainthread
     def tool_change_prompt(self, l):
         # Print is paused by gcode command M6, prompt for tool change
-        self.display("ACTION NEEDED: Manual Tool Change: {}\nWait for machine to stop then you can jog around to change the tool. tap resume to continue".format(l))
+        self.display("ACTION NEEDED: Manual Tool Change:\n Tool: {}\nWait for machine to stop, then you can jog around to change the tool.\n tap resume to continue".format(l))
         # we tell smoothie to suspend so we can save state and move the head around to change the tool
         # we need to issue resume M601 when we click the Resume button
         self.app.comms.write('M600\n')
