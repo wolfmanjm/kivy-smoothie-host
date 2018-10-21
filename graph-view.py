46eb58a8516f69c3e56cc75844bba974d9dc1a79
Jim Morris <morris@wolfman.com>
2018-10-21 13:36:56 +0100

    Initial tempo graph

---
+++ graph-view.py
@@ -0,0 +1,91 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ListProperty
+from kivy.lang import Builder
+from libs.graph import Graph, MeshLinePlot
+from kivy.utils import get_color_from_hex as rgb
+from kivy.clock import Clock
+
+Builder.load_string('''
+#:import rgb kivy.utils.get_color_from_hex
+<GraphView>:
+    Graph:
+        id: graph
+        label_options: { 'color': rgb('444444'),  'bold': True } # color of tick labels and titles
+        background_color: (0, 0, 0, 1)  # back ground color of canvas
+        tick_color: (0,0,1, 1)  # ticks and grid
+        border_color: (0,1,1, 1)  # border drawn around each graph
+        font_size: '8sp'
+        precision: '%.1f' # axis label formatting
+        # xlabel: 'Time (Mins)'
+        # ylabel: 'Temp °C'
+        x_ticks_minor: 1
+        x_ticks_major: 1
+        #y_ticks_minor: 10
+        y_ticks_major: 50
+        y_grid_label: True
+        x_grid_label: True
+        y_ticks_angle: 90
+        padding: 5
+        xlog: False
+        ylog: False
+        x_grid: False
+        y_grid: False
+        xmin: 0
+        xmax: 5
+        ymin: 10
+        ymax: 300
+''')
+
+class GraphView(FloatLayout):
+    def __init__(self, **kwargs):
+        super(GraphView, self).__init__(**kwargs)
+        self.he1_plot= None
+        self.he2_plot= None
+        self.bed_plot= None
+        self.secs= 0
+        self.temp= 20
+        self.clk= None
+
+    def start(self):
+        if self.clk is None:
+            self.clk= Clock.schedule_interval(self.update_points, .1)
+
+        if self.he1_plot is None:
+            self.he1_plot = MeshLinePlot(color=(1,0,0))
+            self.ids.graph.add_plot(self.he1_plot)
+        if self.bed_plot is None:
+            self.bed_plot = MeshLinePlot(color=(0,1,0))
+            self.ids.graph.add_plot(self.bed_plot)
+
+    def stop(self):
+        # if self.clk:
+        #     self.clk.cancel()
+        #     self.clk= None
+        pass
+
+    def update_points(self, *args):
+        if len(self.he1_plot.points) > 300: # 5 minutes
+            del(self.he1_plot.points[0])
+            del(self.bed_plot.points[0])
+            self.ids.graph.xmin += 1/60.
+            self.ids.graph.xmax += 1/60.
+
+        self.he1_plot.points.append((self.secs/60., self.temp))
+        self.temp += 2
+        if self.temp > 200:
+            self.temp= 200
+
+        self.bed_plot.points.append((self.secs/60., self.temp/2.))
+
+        self.secs += 1
+
+
+    # TODO add set points
+    # TODO add he2
+    # TODO customize x axis label to round mins
+
+
+
+if __name__ == '__main__':
+    from kivy.base import runTouchApp
+    runTouchApp(GraphView(width=800))
