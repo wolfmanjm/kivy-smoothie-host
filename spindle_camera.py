6fb75c13a5f9791b25080e3099a091d976e2238a
Jim Morris <morris@wolfman.com>
2019-05-01 19:30:21 +0100

    add invert jog button

--- spindle_camera.py
+++ spindle_camera.py
@@ -4,7 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ToggleButtonBehavior, ButtonBehavior
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, BooleanProperty
 import time
 
 Builder.load_string('''
@@ -61,7 +61,10 @@ Builder.load_string('''
             spacing: 8
             ITogButton:
                 source: "img/cross-mouse.png"
-                on_press: root.toggle_jog(self.state != 'normal')
+                on_state: root.jog = self.state == 'down'
+            ITogButton:
+                source: "img/invert_jog.png"
+                on_state: root.invert_jog = self.state == 'down'
             IButton:
                 source: "img/set_zero.png"
                 on_press: root.setzero()
@@ -83,14 +86,13 @@ class IconButton(ButtonBehavior, Image):
 
 
 class SpindleCamera(Screen):
+    invert_jog = BooleanProperty(False)
+    jog = BooleanProperty(False)
+
     def __init__(self, **kwargs):
         super(SpindleCamera, self).__init__(**kwargs)
         self.app = App.get_running_app()
         self.nfingers = 0
-        self.jog = False
-
-    def toggle_jog(self, on):
-        self.jog = on
 
     def setzero(self):
         self.app.comms.write('G10 L20 P0 X0 Y0\n')
@@ -101,6 +103,7 @@ class SpindleCamera(Screen):
         camera.export_to_png("IMG_{}.png".format(timestr))
 
     def on_touch_down(self, touch):
+        print(self.jog, self.invert_jog)
         if self.jog and self.ids.camera.collide_point(touch.x, touch.y):
             # if within the camera window
             touch.grab(self)
@@ -111,6 +114,7 @@ class SpindleCamera(Screen):
         return super(SpindleCamera, self).on_touch_down(touch)
 
     def on_touch_move(self, touch):
+
         if touch.grab_current is not self:
             return False
 
@@ -126,6 +130,9 @@ class SpindleCamera(Screen):
                 dy = m if touch.dy > 0 else -m
 
             if dx != 0 or dy != 0:
+                if self.invert_jog:
+                    dx = -dx
+                    dy = -dy
                 self.app.comms.write("$J X{} Y{}\n".format(dx, dy))
 
         elif self.nfingers == 4 and touch.ud["n"] == 1:
