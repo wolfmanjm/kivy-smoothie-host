762a9f0a82bd27c296f1dc398fcaed2953f32fbc
alkabal <33371787+alkabal@users.noreply.github.com>
2019-05-12 14:13:20 +0200

    WGB04B button and rotary ok from debug

--- modules/whb04b.py
+++ modules/whb04b.py
@@ -1,4 +1,4 @@
-# implements interface  for the HB04 USB pendant
+# implements interface  for the whb04b USB pendant
 
 from easyhid import Enumeration
 from kivy.logger import Logger
@@ -10,24 +10,24 @@ import math
 import configparser
 import time
 
-hb04 = None
+whb04b = None
 
 
 def start(args=""):
-    global hb04
+    global whb04b
     # print("start with args: {}".format(args))
     pid, vid = args.split(':')
-    hb04 = HB04(int(pid, 16), int(vid, 16))
-    hb04.start()
+    whb04b = WHB04B(int(pid, 16), int(vid, 16))
+    whb04b.start()
     return True
 
 
 def stop():
-    global hb04
-    hb04.stop()
+    global whb04b
+    whb04b.stop()
 
 
-class HB04HID:
+class WHB04BHID:
     PACKET_LEN = 64
     TIMEOUT = 1000  # milliseconds
     CONNECT_RETRIES = 3
@@ -43,6 +43,9 @@ class HB04HID:
     # @vid - Vendor ID of the USB device.
     # @pid - Product ID of the USB device.
     #
+    # USB vendor  ID = 0x10ce
+    # USB product ID = 0xeb93
+    #
     # Returns True on success.
     # Returns False on failure.
     def open(self, vid, pid):
@@ -57,18 +60,18 @@ class HB04HID:
         # return a list of devices based on the search parameters
         devices = en.find(vid=vid, pid=pid, interface=0)
         if not devices:
-            Logger.debug("HB04HID: No matching device found")
+            Logger.debug("WHB04BHID: No matching device found")
             return None
 
         if len(devices) > 1:
-            Logger.debug("HB04HID: more than one device found: {}".format(devices))
+            Logger.debug("WHB04BHID: more than one device found: {}".format(devices))
             return None
 
         # open the device
         self.hid = devices[0]
         self.hid.open()
 
-        Logger.debug("HB04HID: Opened: {}".format(self.hid.description()))
+        Logger.debug("WHB04BHID: Opened: {}".format(self.hid.description()))
         self.opened = True
         return True
 
@@ -94,7 +97,7 @@ class HB04HID:
 
     # Read data from the connected USB device.
     #
-    # @len     - Number of bytes to read. Defaults to PACKET_LEN.
+    # @plen     - Number of bytes to read. Defaults to PACKET_LEN.
     # @timeout - Read timeout, in milliseconds. Defaults to TIMEOUT.
     #
     # Returns the received bytes on success.
@@ -106,86 +109,80 @@ class HB04HID:
     # send feature report, but breaks it into 7 byte packets
     def write(self, data):
         n = 0
-        n += self.hid.send_feature_report(data[0:7], 0x06)
-        n += self.hid.send_feature_report(data[7:14], 0x06)
-        n += self.hid.send_feature_report(data[14:21], 0x06)
-        n += self.hid.send_feature_report(data[21:28], 0x06)
-        n += self.hid.send_feature_report(data[28:35], 0x06)
-        n += self.hid.send_feature_report(data[35:42], 0x06)
+        n += self.hid.send_feature_report(data[0:8], 0x07)
+        n += self.hid.send_feature_report(data[8:16], 0x07)
+        n += self.hid.send_feature_report(data[16:24], 0x07)
         return n
 
 
 # button definitions
 BUT_NONE = 0
-BUT_RESET = 23
-BUT_STOP = 22
-BUT_ORIGIN = 1
-BUT_START = 2
-BUT_REWIND = 3
-BUT_PROBEZ = 4
-BUT_SPINDLE = 12
-BUT_HALF = 6
-BUT_ZERO = 7
-BUT_SAFEZ = 8
-BUT_HOME = 9
-BUT_MACRO1 = 10
-BUT_MACRO2 = 11
-BUT_MACRO3 = 5
-BUT_MACRO6 = 15
-BUT_MACRO7 = 16
-BUT_STEP = 13
+BUT_RESET = 1
+BUT_STOP = 2
+BUT_START = 3
+BUT_FEEDP = 4
+BUT_FEEDM = 5
+BUT_SPINDLEP = 6
+BUT_SPINDLEM = 7
+BUT_MHOME = 8
+BUT_SAFEZ = 9
+BUT_WHOME= 10
+BUT_SPINDLE = 11
+BUT_FN = 12
+BUT_PROBEZ = 13
 BUT_MPG = 14
+BUT_STEP = 15
+BUT_MACRO10 = 16
 
 
-class HB04():
+class WHB04B():
     lcd_data = [
         0xFE, 0xFD, 1,
+        0,           # Mode
         0, 0, 0, 0,  # X WC
         0, 0, 0, 0,  # Y WC
         0, 0, 0, 0,  # Z WC
-        0, 0, 0, 0,  # X MC
-        0, 0, 0, 0,  # Y MC
-        0, 0, 0, 0,  # Z MC
         0, 0,        # F ovr
         0, 0,        # S ovr
-        0, 0,        # F
-        0, 0,        # S
-        0x01,        # step mul
-        0,           # inch/mm
-        0, 0, 0, 0, 0   # padding
+        0, 0, 0, 0,  # padding
     ]
 
     lock = threading.RLock()
     # button look up table
     butlut = {
-        1: "origin",
-        2: "start",
-        3: "rewind",
-        4: "probez",
-        5: "macro3",
-        6: "half",
-        7: "zero",
-        8: "safez",
-        9: "home",
-        10: "macro1",
-        11: "macro2",
-        12: "spindle",
-        15: "macro6",
-        16: "macro7",
+        1: "reset",
+        2: "stop",
+        3: "start",
+        4: "feed+",
+        5: "feed-",
+        6: "spindle+",
+        7: "spindle-",
+        8: "mhome",
+        9: "safez",
+        10: "whome",
+        11: "spindle",
+        12: "fn",
+        13: "probez",
+        14: "mpg",
+        15: "step",
+        16: "macro10",
     }
 
-    alut = {0: 'off', 0x11: 'X', 0x12: 'Y', 0x13: 'Z', 0x18: 'A', 0x15: 'F', 0x14: 'S'}
+    alut = {0x06: 'OFF', 0x11: 'X', 0x12: 'Y', 0x13: 'Z', 0x14: 'A', 0x15: 'B', 0x16: 'C'} # button off + set Axis
+    slut = {0x0d: '0.001', 0x0e: '0.01', 0x0f: '0.1', 0x10: '1', 0x1a: '0', 0x1b: '0', 0x1c: '0'} # button for set step multiplier
+    mlut = {0x0d: '2', 0x0e: '5', 0x0f: '10', 0x10: '30', 0x1a: '60', 0x1b: '100', 0x1c: '0'} # button for set mpg %
+    llut = {0x0d: '0', 0x0e: '0', 0x0f: '0', 0x10: '0', 0x1a: '0', 0x1b: '0', 0x1c: '0.001'} # button for set lead mode
     mul = 1
-    mullut = {0x00: 0, 0x01: 1, 0x02: 5, 0x03: 10, 0x04: 20, 0x05: 30, 0x06: 40, 0x07: 50, 0x08: 100, 0x09: 500, 0x0A: 1000}
+   # mullut = {0x00: 0, 0x01: 1, 0x02: 5, 0x03: 10, 0x04: 20, 0x05: 30, 0x06: 40, 0x07: 50, 0x08: 100, 0x09: 500, 0x0A: 1000}
     macrobut = {}
     f_ovr = 100
     s_ovr = 100
 
     def __init__(self, vid, pid):
-        # HB04 vendor ID and product ID
+        # WHB04B vendor ID and product ID
         self.vid = vid
         self.pid = pid
-        self.hid = HB04HID()
+        self.hid = WHB04BHID()
 
     def twos_comp(self, val, bits):
         """compute the 2's complement of int value val"""
@@ -205,7 +202,7 @@ class HB04():
     def load_macros(self):
         try:
             config = configparser.ConfigParser()
-            config.read('hb04.ini')
+            config.read('whb04b.ini')
             # load user defined macro buttons
             for (key, v) in config.items('macros'):
                 self.macrobut[key] = v
@@ -214,7 +211,7 @@ class HB04():
             self.mul = config.getint("defaults", "multiplier", fallback=8)
 
         except Exception as err:
-            Logger.warning('HB04: WARNING - exception parsing config file: {}'.format(err))
+            Logger.warning('WHB04B: WARNING - exception parsing config file: {}'.format(err))
 
     def handle_button(self, btn, axis):
         name = self.butlut[btn]
@@ -233,20 +230,8 @@ class HB04():
 
         # some buttons have default functions
         cmd = None
-        if btn == BUT_HOME:
-            cmd = "$H"
-        elif btn == BUT_ORIGIN:
-            cmd = "G90 G0 X0 Y0"
-        elif btn == BUT_PROBEZ:
-            cmd = "G38.3 Z-25"
-        elif btn == BUT_ZERO:
-            cmd = "G10 L20 P0 {}0".format(axis)
-        elif btn == BUT_SAFEZ:
-            cmd = "G91 G0 Z20 G90"
-        elif btn == BUT_SPINDLE:
+        if btn == BUT_SPINDLE:
             cmd = "M5" if self.app.is_spindle_on else "M3"
-        elif btn == BUT_HALF:
-            cmd = "G10 L20 P0 {}{}".format(axis, self.app.wpos[ord(axis) - ord('X')] / 2.0)
         elif btn == BUT_START:
             # TODO if running then pause
             self.app.main_window.start_last_file()
@@ -263,25 +248,25 @@ class HB04():
 
         while not self.quit:
             try:
-                # Open a connection to the HB04
+                # Open a connection to the WHB04B
                 if self.hid.open(self.vid, self.pid):
 
-                    Logger.info("HB04: Connected to HID device %04X:%04X" % (self.vid, self.pid))
+                    Logger.info("WHB04B: Connected to HID device %04X:%04X" % (self.vid, self.pid))
 
                     # setup LCD with current settings
                     self.setwcs(self.app.wpos)
-                    self.setmcs(self.app.mpos[0:3])
-                    self.setovr(self.f_ovr, self.s_ovr)
-                    self.setfs(self.app.frr, self.app.sr)
-                    self.setmul(self.mul)
-                    self.update_lcd()
+#                    self.setmcs(self.app.mpos[0:3])
+#                    self.setovr(self.f_ovr, self.s_ovr)
+#                    self.setfs(self.app.frr, self.app.sr)
+#                    self.setmul(self.mul)
+#                    self.update_lcd()
 
                     # get notified when these change
                     self.app.bind(wpos=self.update_wpos)
                     self.app.bind(mpos=self.update_mpos)
                     self.app.bind(fro=self.update_fro)
 
-                    # Infinite loop to read data from the HB04
+                    # Infinite loop to read data from the WHB04B
                     while not self.quit:
                         data = self.hid.recv(timeout=1000)
                         if data is None:
@@ -299,31 +284,33 @@ class HB04():
                             continue
 
                         if data[0] != 0x04:
-                            Logger.error("HB04: Not an HB04 HID packet")
+                            Logger.error("WHB04B: Not an WHB04B HID packet")
                             continue
 
-                        btn_1 = data[1]
-                        btn_2 = data[2]
-                        wheel_mode = data[3]
-                        wheel = self.twos_comp(data[4], 8)
-                        xor_day = data[5]
-                        Logger.debug("HB04: btn_1: {}, btn_2: {}, mode: {}, wheel: {}".format(btn_1, btn_2, self.alut[wheel_mode], wheel))
+                        unused = data[1]
+                        btn_1 = data[2]
+                        btn_2 = data[3]
+                        speed_mode = data[4]
+                        axis_mode = data[5]
+                        wheel = self.twos_comp(data[6], 8)
+                        checksum = data[7]
+                        Logger.debug("whb04b: btn_1: {}, btn_2: {}, speed: {}, axis: {}, wheel: {}".format(btn_1, btn_2, self.slut[speed_mode], self.alut[axis_mode], wheel))
 
                         # handle move multiply buttons
                         if btn_1 == BUT_STEP:
                             self.mul += 1
                             if self.mul > 10:
                                 self.mul = 1
-                            self.setmul(self.mul)
-                            self.update_lcd()
+#                            self.setmul(self.mul)
+#                            self.update_lcd()
                             continue
 
                         if btn_1 == BUT_MPG:
                             self.mul -= 1
                             if self.mul < 1:
                                 self.mul = 10
-                            self.setmul(self.mul)
-                            self.update_lcd()
+#                            self.setmul(self.mul)
+#                            self.update_lcd()
                             continue
 
                         if not self.app.is_connected:
@@ -341,56 +328,57 @@ class HB04():
                         if self.app.main_window.is_printing and not self.app.main_window.is_suspended:
                             continue
 
-                        if wheel_mode == 0:
+                        if axis_mode == 6:
                             # when OFF all other buttons are ignored
                             continue
 
-                        axis = self.alut[wheel_mode]
+                        axis = self.alut[axis_mode]
+                        f_over = self.slut[speed_mode]
 
                         # handle other fixed and macro buttons
                         if btn_1 != 0 and self.handle_button(btn_1, axis):
                             continue
 
                         if wheel != 0:
-                            if axis == 'F':
-                                # adjust feed override
-                                self.f_ovr += wheel
-                                if self.f_ovr < 10:
-                                    self.f_ovr = 10
-                                self.setovr(self.f_ovr, self.s_ovr)
-                                self.update_lcd()
-                                continue
-                            if axis == 'S':
-                                # adjust S override, laser power? (TODO maybe this is tool speed?)
-                                self.s_ovr += wheel
-                                if self.s_ovr < 1:
-                                    self.s_ovr = 1
-                                self.setovr(self.f_ovr, self.s_ovr)
-                                self.update_lcd()
-                                continue
+#                            if axis == 'F':
+#                                # adjust feed override
+#                                self.f_ovr += wheel
+#                                if self.f_ovr < 10:
+#                                    self.f_ovr = 10
+#                                self.setovr(self.f_ovr, self.s_ovr)
+#                                self.update_lcd()
+#                                continue
+#                            if axis == 'S':
+#                                # adjust S override, laser power? (TODO maybe this is tool speed?)
+#                                self.s_ovr += wheel
+#                                if self.s_ovr < 1:
+#                                    self.s_ovr = 1
+#                                self.setovr(self.f_ovr, self.s_ovr)
+#                                self.update_lcd()
+#                                continue
 
-                            # must be one of XYZA so send jogging command
+                            # must be one of XYZABC so send jogging command
                             # velocity_mode:
                             # step= -1 if wheel < 0 else 1
                             # s = -wheel if wheel < 0 else wheel
                             # if s > 5: s == 5 # seems the max realistic we get
                             # speed= s/5.0 # scale where 5 is max speed
                             step = wheel  # speed of wheel will move more increments rather than increase feed rate
-                            dist = 0.001 * step * self.mullut[self.mul]
+                            dist = 0.001 * step * self.slut[self.mul]
                             speed = 1.0
                             self.app.comms.write("$J {}{} F{}\n".format(axis, dist, speed))
                             # print("$J {}{} F{}\n".format(axis, dist, speed))
 
-                    # Close the HB04 connection
+                    # Close the WHB04B connection
                     self.hid.close()
 
-                    Logger.info("HB04: Disconnected from HID device")
+                    Logger.info("WHB04B: Disconnected from HID device")
 
                 else:
-                    Logger.debug("HB04: Failed to open HID device %04X:%04X" % (self.vid, self.pid))
+                    Logger.debug("whb04b: Failed to open HID device %04X:%04X" % (self.vid, self.pid))
 
             except Exception:
-                Logger.error("HB04: Exception - {}".format(traceback.format_exc()))
+                Logger.error("WHB04B: Exception - {}".format(traceback.format_exc()))
                 if self.hid.opened:
                     self.hid.close()
 
@@ -401,7 +389,7 @@ class HB04():
                 # retry connection in 5 seconds unless we were asked to quit
                 time.sleep(5)
 
-    # converts a 16 bit value to little endian bytes suitable for HB04 protocol
+    # converts a 16 bit value to little endian bytes suitable for WHB04B protocol
     def to_le(self, x, neg=False):
         lo = abs(x) & 0xFF
         hi = (abs(x) >> 8) & 0xFF
