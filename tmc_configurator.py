e569facd47075e6fc93058c2cc797c7ead6706c8
Jim Morris <morris@wolfman.com>
2022-10-02 19:07:01 +0100

    add hysterises to sliders

--- tmc_configurator.py
+++ tmc_configurator.py
@@ -7,11 +7,9 @@ from kivy.properties import NumericProperty, BooleanProperty
 '''
     M911.3 S1 [Unnn Vnnn Wnnn Xnnn Ynnn] - setSpreadCycleChopper
               U=constant_off_time, V=blank_time, W=hysteresis_start, X=hysteresis_end, Y=hysteresis_decrement
-
+                2...15               0..54         1...8               -3..12            0..3
     TODO
-        select motor or all motors
         dump registers for inclusion in config (or set config)
-        add hysteresis sliders for setSpreadCycleChopper
         maybe allow setting of ConstantOffTimeChopper
 '''
 
@@ -62,6 +60,57 @@ Builder.load_string('''
                     max: 54
                     value: root.blank_time
                     on_value: root.set_blank_time(self.value)
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: 'Hyst start'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: str(int(s3.value))
+                Slider:
+                    id: s3
+                    orientation: 'vertical'
+                    min: 1
+                    max: 8
+                    value: root.hysteresis_start
+                    on_value: root.set_hysteresis_start(self.value)
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: 'Hyst end'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: str(int(s4.value))
+                Slider:
+                    id: s4
+                    orientation: 'vertical'
+                    min: -3
+                    max: 12
+                    value: root.hysteresis_end
+                    on_value: root.set_hysteresis_end(self.value)
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: 'Hyst dec'
+                Label:
+                    size_hint_y: None
+                    height: 40
+                    text: str(int(s5.value))
+                Slider:
+                    id: s5
+                    orientation: 'vertical'
+                    min: 0
+                    max: 3
+                    value: root.hysteresis_dec
+                    on_value: root.set_hysteresis_dec(self.value)
 
         BoxLayout:
             orientation: 'horizontal'
@@ -125,8 +174,13 @@ Builder.load_string('''
 class TMCConfigurator(Screen):
     constant_off_time = NumericProperty(5)
     blank_time = NumericProperty(54)
+    hysteresis_start = NumericProperty(5)
+    hysteresis_end = NumericProperty(0)
+    hysteresis_dec = NumericProperty(0)
+
     pfd = BooleanProperty(True)
     rot = BooleanProperty(False)
+
     enabled_list = {'X': True, 'Y': True, 'Z': True, 'A': True}
     motor_lut = {'X': 0, 'Y': 1, 'Z': 2, 'A': 3}
 
@@ -154,46 +208,47 @@ class TMCConfigurator(Screen):
 
     def set_constant_off_time(self, v):
         self.constant_off_time = int(v)
-        if self.are_all_enabled():
-            self.send_command('M911.3 S1 U{}'.format(self.constant_off_time))
-        else:
-            for a in self.enabled_list:
-                if self.enabled_list[a]:
-                    self.send_command('M911.3 P{} S1 U{}'.format(self.motor_lut[a], self.constant_off_time))
+        self.send_M911_command('S1 U{}'.format(self.constant_off_time))
 
     def set_blank_time(self, v):
         self.blank_time = int(v)
-        if self.are_all_enabled():
-            self.send_command('M911.3 S1 V{}'.format(self.blank_time))
-        else:
-            for a in self.enabled_list:
-                if self.enabled_list[a]:
-                    self.send_command('M911.3 P{} S1 V{}'.format(self.motor_lut[a], self.blank_time))
+        self.send_M911_command('S1 V{}'.format(self.blank_time))
+
+    def set_hysteresis_start(self, v):
+        self.hysteresis_start = int(v)
+        self.send_M911_command('S1 W{}'.format(self.hysteresis_start))
+
+    def set_hysteresis_end(self, v):
+        self.hysteresis_end = int(v)
+        self.send_M911_command('S1 X{}'.format(self.hysteresis_end))
+
+    def set_hysteresis_dec(self, v):
+        self.hysteresis_dec = int(v)
+        self.send_M911_command('S1 Y{}'.format(self.hysteresis_dec))
 
     def set_pfd(self, v):
         self.pfd = v
-        if self.are_all_enabled():
-            self.send_command('M911.3 S6 Z{}'.format("1" if self.pfd else "0"))
-        else:
-            for a in self.enabled_list:
-                if self.enabled_list[a]:
-                    self.send_command('M911.3 P{} S6 Z{}'.format(self.motor_lut[a], "1" if self.pfd else "0"))
+        self.send_M911_command('S6 Z{}'.format("1" if self.pfd else "0"))
 
     def set_rot(self, v):
         self.rot = v
-        if self.are_all_enabled():
-            self.send_command('M911.3 S2 Z{}'.format("1" if self.rot else "0"))
-        else:
-            for a in self.enabled_list:
-                if self.enabled_list[a]:
-                    self.send_command('M911.3 P{} S2 Z{}'.format(self.motor_lut[a], "1" if self.rot else "0"))
+        self.send_M911_command('S2 Z{}'.format("1" if self.rot else "0"))
 
     def save_settings(self):
+        send_command('M911')
         # self.app.comms.write("config-set sd {} {}\n".format(k, v))
         pass
 
     def close(self):
         self.manager.current = 'main'
+
+    def send_M911_command(self, cmd):
+        if self.are_all_enabled():
+            self.send_command('M911.3 {}'.format(cmd))
+        else:
+            for a in self.enabled_list:
+                if self.enabled_list[a]:
+                    self.send_command('M911.3 {} P{}'.format(cmd, self.motor_lut[a]))
 
     def send_command(self, args):
         App.get_running_app().comms.write('{}\n'.format(args))
