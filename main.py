d10281be6ee73e2bf736c8d25eba82e9a305bfd9
Jim Morris <morris@wolfman.com>
2022-12-09 10:40:41 +0000

    Allow python version 3.10.x to work

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ import importlib
 import signal
 
 # we must have Python version between 3.5 and 3.9 inclusive
-assert (3, 10) >= sys.version_info >= (3, 5), "Python version needs to be >= 3.5 and <= 3.9, you have {}".format(sys.version)
+assert (3, 10, 99) >= sys.version_info >= (3, 5), "Python version needs to be >= 3.5 and <= 3.10.x, you have {}".format(sys.version)
 
 kivy.require('1.11.0')
 
