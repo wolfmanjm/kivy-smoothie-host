5af29afa36cbd6f23da2fed9e2e4602011bfcaf8
Jim Morris <morris@wolfman.com>
2023-03-07 21:56:16 +0000

    tweaks

--- spindle_handler.py
+++ spindle_handler.py
@@ -87,9 +87,12 @@ class SpindleHandler():
 
             pwm = p1 + (p2 - p1) * ((mrpm - r1) / (r2 - r1))
 
-        # for debugging
         Logger.debug(f"SpindleHandler: Spindle RPM of {srpm} is motor RPM of {mrpm:1.2f} which is PWM of {pwm:1.2f}")
-        App.get_running_app().main_window.async_display(f"Spindle RPM of {srpm} is motor RPM of {mrpm:1.2f} which is PWM of {pwm:1.2f}")
+
+        # FIXME for debugging remove when done
+        app = App.get_running_app()
+        if app is not None:
+            app.main_window.async_display(f"Spindle RPM of {srpm} is motor RPM of {mrpm:1.2f} which is PWM of {pwm:1.2f}")
         return pwm
 
 
