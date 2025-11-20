394527907eb741a9e8bbe14cef8407e16fb3729c
Jim Morris <morris@wolfman.com>
2025-11-20 20:37:26 +0000

    fix bugs in size

--- scripts/probe.rb
+++ scripts/probe.rb
@@ -215,7 +215,8 @@ def probe_size
     r2= probe(:x, -20) # probe left
 
     width= r2.x - r1.x - d1
-    STDERR.puts "Width= #{width}, expected= #{$options.width}, difference= #{$options.width-width}, percentage= #{$options.width * 100.0 / ($options.width-width)}"
+    diff = $options.width - width
+    STDERR.puts "Width= #{width}, expected= #{$options.width}\ndifference= #{diff}, percentage= #{diff * 100.0 / $options.width}"
 
     if $options.length > 0
         # center in X and in front of Y face
@@ -228,14 +229,19 @@ def probe_size
         r2= probe(:y, -20) # probe negative Y
 
         length= r2.y - r1.y - d1
-        STDERR.puts "Length= #{length}, expected= #{$options.length}, difference= #{$options.length-length}, percentage= #{$options.length * 100.0 / ($options.length-length)}"
+        diff = $options.length - length
+        STDERR.puts "Length= #{length}, expected= #{$options.length}\ndifference= #{diff}, percentage= #{diff * 100.0 / $options.length}"
 
         # center in Y
         moveBy(y: -length/2.0-d2, down: false)
+
+        STDERR.puts "Size= #{length} x #{width} mm"
+        STDERR.puts "Size= #{length/25.4} x #{width/25.4} in"
+    else
+        # center in X
+        moveBy(x: -width/2.0 - d2)
     end
 
-    STDERR.puts "Size= #{length} x #{width} mm"
-    STDERR.puts "Size= #{length/25.4} x #{width/25.4} in"
 end
 
 def probe_center
