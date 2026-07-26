7a4f71e19ff852854132731b26b876ac42246e0c
Jim Morris <morris@wolfman.com>
2026-07-26 15:24:05 +0100

    allow for backlash when probing inner circle, by moving past center then moving back

--- tool_scripts.py
+++ tool_scripts.py
@@ -157,8 +157,10 @@ class ToolScripts():
 
             diam = r1['X'] - r2['X']
 
-            # center in X
-            self._moveby(x=diam / 2.0)
+            # center in X, allow for backlash by going past and coming back
+            d = diam / 2.0
+            self._moveby(x=d + 1)
+            self._moveby(x=-1)
             self._wait()
 
             # probe back
@@ -173,8 +175,10 @@ class ToolScripts():
 
             diam = r1['Y'] - r2['Y']
 
-            # center in Y
-            self._moveby(y=diam / 2.0)
+            # center in Y, allow for backlash by going past and coming back
+            d = diam / 2.0
+            self._moveby(y=d + 1)
+            self._moveby(y=-1)
             self._wait()
 
             # tell us the approx diameter
