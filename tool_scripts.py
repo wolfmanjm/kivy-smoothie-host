6d67af27e8ccfbc624ad741a3a8c258e0bbf93b9
Jim Morris <morris@wolfman.com>
2026-08-02 18:54:51 +0100

    add bsvladh comp to find center

--- tool_scripts.py
+++ tool_scripts.py
@@ -182,8 +182,7 @@ class ToolScripts():
             self._wait()
 
             # tell us the approx diameter
-            self.app.main_window.async_display(
-                "Diameter is {}, plus the tool diameter".format(diam))
+            self.app.main_window.async_display(f"Diameter is {diam}, plus the tool diameter")
 
         except Exception as msg:
             Logger.info("Tools: Exception - {}".format(traceback.format_exc()))
